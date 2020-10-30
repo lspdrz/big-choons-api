52a1072dd4ebe3c6caad08caaf13872cfe182b0d
Luis Pedraza <luis@lspdrz.com>
2020-10-30 14:07:14 +0300

    added Procfile and other heroku settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn big_choons_api.wsgi --log-file -
--- big_choons_api/settings.py
+++ big_choons_api/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import django_heroku
 from pathlib import Path
 import os
 import environ
@@ -132,3 +133,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
