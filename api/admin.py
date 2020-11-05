8ccecff42e7ab00528df7d0e11e178d4407b6b44
Luis Pedraza <luis@lspdrz.com>
2020-11-05 13:42:22 +0300

    new model structure

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import User, Track, UserTrack, UserFollowing
+from .models import Profile, Track, UserTrack, UserFollowing
 
 # Register your models here.
-admin.site.register(User)
+admin.site.register(Profile)
 admin.site.register(Track)
 admin.site.register(UserTrack)
 admin.site.register(UserFollowing)
--- api/models.py
+++ api/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.translation import gettext_lazy as _
 
 
@@ -10,13 +13,20 @@ class Base(models.Model):
         abstract = True
 
 
-class User(Base):
-    username = models.CharField(max_length=30)
-    name = models.CharField(max_length=60)
-    email = models.CharField(max_length=60, unique=True)
+class Profile(Base):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
 
-    def __str__(self):
-        return self.username
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
 
 class Track(Base):
@@ -39,8 +49,8 @@ class Track(Base):
 
 
 class UserTrack(Base):
-    user = models.ForeignKey(
-        "User", related_name="user", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name="user",
+                             on_delete=models.CASCADE)
     track = models.ForeignKey(
         "Track", related_name="track", on_delete=models.CASCADE)
     notes = models.CharField(max_length=280, null=True)
@@ -48,7 +58,7 @@ class UserTrack(Base):
 
 
 class UserFollowing(Base):
-    user_id = models.ForeignKey(
-        "User", related_name="following", on_delete=models.CASCADE)
-    following_user_id = models.ForeignKey(
-        "User", related_name="followers", on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, related_name="following", on_delete=models.CASCADE)
+    following_user = models.ForeignKey(
+        User, related_name="followers", on_delete=models.CASCADE)
