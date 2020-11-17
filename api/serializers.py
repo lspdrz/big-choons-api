d46363ab44a11d7d5401a7336e280dee2b2c1ae1
Luis Pedraza <luis@lspdrz.com>
2020-11-17 16:52:35 +0300

    amended user serializer to include new profile

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,21 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Profile, Track
 
-from .models import User, Track
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('id', 'bio', 'location', 'birth_date')
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    profile = ProfileSerializer()
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'name', 'email')
+        fields = ('id', 'username', 'first_name',
+                  'last_name', 'email', 'profile')
 
 
 class TrackSerializer(serializers.HyperlinkedModelSerializer):
